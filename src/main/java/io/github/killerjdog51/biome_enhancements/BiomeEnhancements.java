11163e6d6363cfac4eedc0c5a976729d7885c4e4
killerjdog51 <killerjdog51@gmail.com>
2020-07-29 16:39:26 -0500

    Hello World, Biome Enhancement is first created.

---
+++ src/main/java/io/github/killerjdog51/biome_enhancements/BiomeEnhancements.java
@@ -0,0 +1,17 @@
+package io.github.killerjdog51.biome_enhancements;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BiomeEnhancements implements ModInitializer
+{
+
+	public static final String MOD_ID = "biome_enhancements";
+	
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Initializing Biome Enhancements");
+
+	}
+
+}
